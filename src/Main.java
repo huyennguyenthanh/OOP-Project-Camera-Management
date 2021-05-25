d2d753d7bcbfdb30657c61b1fe88e45a4ee56326
huyennguyenthanh <sakuran1707@gmail.com>
2021-05-26 00:14:13 +0700

    main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 
 import java.io.FileNotFoundException;
 
-import javax.swing.JOptionPane;
-
 import Exception.ExistedObjectException;
-import controller.*;
-import view.*;
-import model.*;
-public class ReadView {
+
+
+public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, ExistedObjectException {
 		// TODO Auto-generated method stub
